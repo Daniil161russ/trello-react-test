95c5516127d19573d23359d1cd0bfda26dbdd242
Boyko Daniil (A) <b84271351@china.huawei.com>
2022-10-11 18:17:47 +0300

    add test data, mapping, sort cards and columns

--- src/components/BoardContent/BoardContent.js
+++ src/components/BoardContent/BoardContent.js
@@ -1,16 +1,42 @@
 import Column from '../Column/Column';
+import { data } from '../../actions/data';
+import { useEffect, useState } from 'react';
+import _ from 'lodash';
+import { mapOrder } from '../../utilities/sorts';
+
 import './BoardContent.scss';
 
 const BoardContent = () => {
+    const [board, setBoard] = useState({});
+    const [columns, setColumns] = useState([]);
+
+    useEffect(() => {
+        const boardData = data.boards.find((item) => item.id === 'board-1');
+
+        if (boardData) {
+            setBoard(boardData);
+
+            setColumns(mapOrder(boardData.columns, boardData.columnOrder, 'id'));
+        }
+    }, []);
+
+    if (_.isEmpty(board)) {
+        return (
+            <>
+                <div className='not-found'>Board not found</div>
+            </>
+        )
+    }
+
     return (
        <>
             <div className='board-columns'>
-                <Column />
-                <Column />
-                <Column />
-                <Column />
-                <Column />
-                <Column />
+                {columns && columns.length && columns.map((column, index) => {
+                    return (
+                        <Column key={column.id} column={column} />
+                    )
+                })}
+                
             </div>
        </> 
     )
--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,25 +1,21 @@
-import Task from '../Task/Task';
+import Card from '../Card/Card';
+import { mapOrder } from '../../utilities/sorts';
 import './Column.scss';
 
-const Column = () => {
+const Column = (props) => {
+
+    const { column } = props;
+    const cards = mapOrder(column.cards, column.cardOrder, 'id');
     return (
        <>
             <div className='column'>
-                <header>Title</header>
-                <ul className='task-list'>
-                    <Task />
-                    {/* <li className='task-item'>Second</li>
-                    <li className='task-item'>Third</li>
-                    <li className='task-item'>First</li>
-                    <li className='task-item'>Second</li>
-                    <li className='task-item'>Third</li>
-                    <li className='task-item'>First</li>
-                    <li className='task-item'>Second</li>
-                    <li className='task-item'>Third</li>
-                    <li className='task-item'>First</li>
-                    <li className='task-item'>Second</li>
-                    <li className='task-item'>Third</li>
-                    <li className='task-item'>First</li> */}
+                <header>{ column.title }</header>
+                <ul className='card-list'>
+                    { cards && cards.length > 0 && cards.map((card, index) => {
+                        return (
+                            <Card key={card.id} card={card}/>
+                        )
+                    })}
                 </ul>
                 <footer>Add another card</footer>
             </div>
